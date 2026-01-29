5df603a70dced9ed75dbc0e74e10566aebe95f75
Akhil Singh <200789891+akhilsingh01@users.noreply.github.com>
2026-01-29 19:44:33 +0530

    chore: improve naming and documentation

--- src/app/math_utils.py
+++ src/app/math_utils.py
@@ -1,14 +1,18 @@
 # Function : add() For adding two numbers
 
+"""
+Provide a function for adding two numbers.
+"""
 
-def add(a: int, b: int) -> int:
+
+def add(first_number: int, second_number: int) -> int:
     """Return the sum of two numbers.
 
     Parameters:
-        a (int): The first number.
-        b (int): The second number.
+        first_number (int): The first number.
+        second_number (int): The second number.
 
     Returns:
-        int: The sum of a and b.
+        int: The sum of first_number and second_number.
     """
-    return a + b
+    return first_number + second_number
--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,20 +1,23 @@
 import pytest
 from app.math_utils import add
 
-def test_add_two_positive_numbers():
-    '''
+
+def test_add_two_positive_numbers() -> None:
+    """
     Test case for when adding two positive number.
-    '''
-    assert add(2,2) == 4
+    """
+    assert add(2, 2) == 4
 
-def test_add_one_positive_one_zero():
-    '''
+
+def test_add_one_positive_one_zero() -> None:
+    """
     Test case for when adding one positive number and zero.
-    '''
-    assert add(2,0) == 2
+    """
+    assert add(2, 0) == 2
 
-def test_add_two_negative_numbers():
-    '''
+
+def test_add_two_negative_numbers() -> None:
+    """
     Test case for when adding two negative number.
-    '''
-    assert add(-2,-3) == -5
+    """
+    assert add(-2, -3) == -5
