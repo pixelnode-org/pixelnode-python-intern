a38c14e58219999baa3200444e4fa9051de2ba2a
Akhil Singh <200789891+akhilsingh01@users.noreply.github.com>
2026-01-30 21:08:51 +0530

    test: added two more edge cases

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,23 +1,38 @@
 import pytest
-from app.math_utils import add
+from src.app.math_utils import add
 
 
 def test_add_two_positive_numbers() -> None:
     """
-    Test case for when adding two positive number.
+    Test case 1: for when adding two positive number.
     """
     assert add(2, 2) == 4
 
 
 def test_add_one_positive_one_zero() -> None:
     """
-    Test case for when adding one positive number and zero.
+    Test case 2: for when adding one positive number and zero.
     """
     assert add(2, 0) == 2
 
 
 def test_add_two_negative_numbers() -> None:
     """
-    Test case for when adding two negative number.
+    Test case 3: for when adding two negative number.
     """
     assert add(-2, -3) == -5
+
+def test_add_raises_type_error_for_mixes_types():
+    """
+    Test case 4: for when adding different types of value other then int.
+    """
+    with pytest.raises(TypeError):
+        add("a",0)
+
+def test_add_raises_type_error_for_strings():
+    """
+    Test case 5: for when adding two string instead of int
+    """
+    with pytest.raises(TypeError):
+        add("a","g")
+        
