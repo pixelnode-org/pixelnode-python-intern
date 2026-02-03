a9b70727522a9f13a5cc640e9143a3d6ce58fca5
ThePixelNode <Social@pixelnode.in>
2026-02-03 20:18:36 +0530

    Delete src/app directory

--- src/app/math_utils.py
+++
@@ -1,9 +0,0 @@
-#Function : add() For adding two numbers 
-
-def add(a:int, b:int) -> int:
-    """Return the sum of two numbers.    
-    Parameters:        
-        a (int): The first number.        
-        b (int): The second number.    
-    Returns:        int: The sum of a and b.    """
-    return a+b
