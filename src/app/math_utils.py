48cdcd1d8822370fe6c11bd0e4a0ea909170a390
Harshit Mishra <113508655+Harshitmishra09@users.noreply.github.com>
2026-02-06 21:39:06 +0530

    feat: implement subtract function and add test cases

--- src/app/math_utils.py
+++ src/app/math_utils.py
@@ -13,11 +13,9 @@ def validate_integers(first_number, second_number):
     """
     if type(first_number) is not int or type(second_number) is not int:
         raise TypeError("Both inputs must be integers.")
-    
 
-"""
-Provide a function for adding two numbers.
-"""
+
+# Provide a function for adding two numbers.
 
 
 def add(first_number: int, second_number: int) -> int:
@@ -31,6 +29,22 @@ def add(first_number: int, second_number: int) -> int:
         int: The sum of first_number and second_number.
     """
     validate_integers(first_number, second_number)
-    
+
     return first_number + second_number
 
+
+# Provide a function for subtracting two numbers.
+
+
+def sub(first_number: int, second_number: int) -> int:
+    """Return the difference of two numbers.
+    Parameters:
+        first_number (int): The first number.
+        second_number (int): The second number.
+
+    Returns:
+        int: The difference of first_number and second_number."""
+
+    validate_integers(first_number, second_number)
+
+    return first_number - second_number
--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,5 +1,9 @@
 import pytest
-from src.app.math_utils import add
+from src.app.math_utils import add, sub, validate_integers
+
+###############################################
+# Test case function for Addition
+##############################################
 
 
 def test_add_two_positive_numbers():
@@ -16,24 +20,53 @@ def test_add_two_negative_numbers():
     # Test case 3: Both negative numbers
     assert add(-2, -3) == -5
 
-def test_add_raises_type_error_for_strings():
-    # Test case 4: Both inputs are strings
-    with pytest.raises(TypeError):
-        add("a", "a")
 
-def test_add_raises_type_error_for_mixed_types_strings():
-    # Test case 5: One input is string, other is integer
-    with pytest.raises(TypeError):
-        add("a", 0)
-        add(0, "a")
+###############################################
+# Test case function for Subtraction
+##############################################
 
-def test_add_raises_type_error_for_boolean():
-    # Test case 6: Both input is boolean
-    with pytest.raises(TypeError):
-        add(True, False)
 
-def test_add_raises_type_error_for_mixed_types_boolean():
-    # Test case 7: One input is boolean, other is integer
+def test_sub_two_positive_numbers():
+    # Test Case 4: For two positive numebers
+    assert sub(2, 2) == 0
+
+
+def test_sub_one_positive_one_zero():
+    # Test case 5: one positive and one zero number edge case
+    assert sub(2, 0) == 2
+
+
+def test_sub_two_negative_numbers():
+    # Test case 6: Both negative numbers
+    assert sub(-2, -3) == 1
+
+
+############################################################
+# Error Handling function for Math Utility module
+###########################################################
+
+
+def test_validate_raises_type_error_for_strings():
+    # Test case 7: Both inputs are strings
     with pytest.raises(TypeError):
-        add(True, 5)
-        add(5, True)
+        validate_integers("a", "a")
+
+
+def test_validate_raises_type_error_for_mixed_types_strings():
+    # Test case 8: One input is string, other is integer
+    with pytest.raises(TypeError):
+        validate_integers("a", 0)
+        validate_integers(0, "a")
+
+
+def test_validate_raises_type_error_for_boolean():
+    # Test case 9: Both input is boolean
+    with pytest.raises(TypeError):
+        validate_integers(True, False)
+
+
+def test_validate_raises_type_error_for_mixed_types_boolean():
+    # Test case 10: One input is boolean, other is integer
+    with pytest.raises(TypeError):
+        validate_integers(True, 5)
+        validate_integers(5, True)
