65cbf5c7b0424dbee1054d64280e72ced80779c6
Akhil Singh <200789891+akhilsingh01@users.noreply.github.com>
2026-02-06 21:31:03 +0530

    feat: implement subtract function and add test cases

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,53 +1,75 @@
 import pytest
 from src.app.math_utils import add
+from src.app.math_utils import subtract
 from src.app.math_utils import validate_integers
 
-
+# Test cases for add function
 def test_add_two_positive_numbers() -> None:
     """
-    Test case 1: for when adding two positive number.
+    Case: for when adding two positive number.
     """
     assert add(2, 2) == 4
 
 
 def test_add_one_positive_one_zero() -> None:
     """
-    Test case 2: for when adding one positive number and zero.
+    Case: for when adding one positive number and zero.
     """
     assert add(2, 0) == 2
 
 
 def test_add_two_negative_numbers() -> None:
     """
-    Test case 3: for when adding two negative number.
+    Case: for when adding two negative number.
     """
     assert add(-2, -3) == -5
 
-def test_add_raises_type_error_for_mixes_types_strings():
+# Test cases for subtract function
+def test_subtract_two_positive_numbers() -> None:
     """
-    Test case 4: for when adding different types of value with strings other then int.
+    Case: for when subtract two positive number.
     """
-    with pytest.raises(TypeError):
-        add("a",0)
+    assert subtract(2, 2) == 0
 
-def test_add_raises_type_error_for_strings():
+def test_subtract_one_positive_one_zero() -> None:
     """
-    Test case 5: for when adding two string instead of int
+    Case: for when subtract one positive number and zero.
+    """
+    assert subtract(2, 0) == 2
+
+
+def test_subtract_two_negative_numbers() -> None:
+    """
+    Case: for when subtract two negative number.
+    """
+    assert subtract(-2, -3) == 1
+
+
+def test_validate_integers_raises_type_error_for_mixes_types_strings() -> None:
+    """
+    Case: for when validating different types of value with strings other then int.
     """
     with pytest.raises(TypeError):
-        add("a","g")
+        validate_integers("a",0)
+
+def test_validate_integers_raises_type_error_for_strings() -> None:
+    """
+    Case: for when validating two string instead of int
+    """
+    with pytest.raises(TypeError):
+        validate_integers("a","g")
         
-def test_add_raises_type_error_for_boolean():
+def test_validate_integers_raises_type_error_for_boolean() -> None:
     """
-    Test case 6: for when adding two boolean instead of int
+    Case: for when validating two boolean instead of int
     """
     with pytest.raises(TypeError):
-        add(True,True)
+        validate_integers(True,True)
 
-def test_add_raises_type_error_for_mixes_types_boolean():
+def test_validate_integers_raises_type_error_for_mixes_types_boolean() -> None:
     """
-    Test case 7: for when adding different types of value with boolean other then int.
+    Case: for when validating different types of value with boolean other then int.
     """
     with pytest.raises(TypeError):
-        add(0,True)
-        add(True,0)
+        validate_integers(0,True)
+        validate_integers(True,0)
