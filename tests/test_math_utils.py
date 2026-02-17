338da73fe89a28a5b340ba08292b74fb58b6bacd
Ayush Rawat <179118566+AyushRawat7878@users.noreply.github.com>
2026-02-17 19:41:06 +0530

    Refactor: add PEP 257 docstrings, improve naming, format with black

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,15 +1,15 @@
 import pytest
 from src.app.math_utils import add
+
+
 ############################
 # valid input tests
 ############################
-@pytest.mark.parametrize("num1, num2, expected", [
-    (2, 3, 5),
-    (5, 0, 5),
-    (-1, -1, -2)
-])
-def test_add_returns_correct_result_for_valid_integers(num1:int, num2:int, expected:int)->None:
-    """"
-    verify that add()returns the correct results for valid integer inputs 
+@pytest.mark.parametrize("num1, num2, expected", [(2, 3, 5), (5, 0, 5), (-1, -1, -2)])
+def test_add_returns_correct_result_for_valid_integers(
+    num1: int, num2: int, expected: int
+) -> None:
+    """ "
+    verify that add()returns the correct results for valid integer inputs
     """
-    assert add (num1, num2) ==  expected
+    assert add(num1, num2) == expected
